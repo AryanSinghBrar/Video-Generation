8d529347576984c9c924cd720427d03ca77a58c2
Aryan <abrar82@uwo.ca>
2023-11-03 22:51:57 -0400

    Adding web scraping, needs more work

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import requests
-import bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
+url = "https://time.com/6315607/bryan-johnsons-quest-for-immortality/" # url of target website
+html = requests.get(url) # This gets the html code of the TB (target website = TB)
+
+s = BeautifulSoup(html.content, 'html.parser')
